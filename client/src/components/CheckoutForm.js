a2f614b1ffcd5b48637bc60ca09ecc92e263678c
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-06-05 11:59:44 -0400

    Completed the PlantList component and set up the CheckoutForm using the useForm custom hook

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "../hooks/useForm"
 
 const initialValue = {
   firstName: "",
@@ -15,11 +16,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges, clearForm] = useForm('Checkout Form', initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,7 +67,7 @@ const CheckoutForm = (props) => {
           Zip:
           <input name="zip" value={values.zip} onChange={handleChanges} />
         </label>
-        <button>Checkout</button>
+        <button onClick={clearForm}>Checkout</button>
       </form>
 
       {showSuccessMessage && (
